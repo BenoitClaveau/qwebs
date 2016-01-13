c74244e1b0902b256fa52980ef4275bc00de51e2
Benoît Claveau <beny78@gmail.com>
2016-01-13 10:35:19 +0100

    use promise

--- tests/loaders/assets.spec.js
+++ tests/loaders/assets.spec.js
@@ -8,12 +8,15 @@ describe("assetsLoader", function () {
         var cfg = {
             folder: "public"
         };
-        var qwebs = new Qwebs({ dirname: __dirname, config: cfg });
         
-        return assetsLoader.load(qwebs).then(function(assets) {
-            expect(assets.length).toEqual(2);            
-            expect(assets[0].route).toEqual("/assets/user.svg");
-            expect(assets[1].route).toEqual("/main.html");
+        new Qwebs({ dirname: __dirname, config: cfg }).then(function(qwebs) {
+        
+            return assetsLoader.load(qwebs).then(function(assets) {
+                expect(assets.length).toEqual(2);            
+                expect(assets[0].route).toEqual("/assets/user.svg");
+                expect(assets[1].route).toEqual("/main.html");
+            });
+            
         }).catch(function (error) {
             expect(error.stack).toBeNull();
         }).finally(done);
