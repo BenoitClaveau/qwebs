6d44435c0e37a5575f9488d7826cf7e464d400cf
Benoît Claveau <benoit.claveau@gmail.com>
2016-11-03 13:29:27 +0100

    delete

--- tests/routes/delete.spec.js
+++ tests/routes/delete.spec.js
@@ -6,33 +6,37 @@
 "use strict";
 
 const Qwebs = require("../../lib/qwebs");
-const Delete = require('../../lib/routes/delete');
+const http = require("http");
+const request = require('request');
 
-describe("delete", () => {
+describe("post", () => {
 
     it("create", done => {
+        let server = null;
         return Promise.resolve().then(() => {
             let $qwebs = new Qwebs({ dirname: __dirname, config: {}});
-            $qwebs.inject("$info", "../services/info");            
+            
+            $qwebs.inject("$info", "../services/info");
+            $qwebs.delete("/save", "$info", "delete");
 
             return $qwebs.load().then(() => {
-                let del = new Delete($qwebs, "/delete");
-                del.register("$info", "delete");
-                del.load();
+                server = http.createServer((request, response) => {
+                    return $qwebs.invoke(request, response).then(res => {
+                        expect(res.status).toBe("saved");
+                    }).catch(error => {
+                        expect(error).toBeNull();
+                    }).then(() => {
+                        done();
+                    });
+                }).listen(1337);
                 
-                let request = {
-                    url: "/delete",
-                    headers: {
-                    }
-                };
-                let response = {
-                };
-
-                //return post.invoke(request, response);
+                let $client = $qwebs.resolve("$client");
+                $client.delete("http://localhost:1337/delete", { login: "test" });
             });
         }).catch(error => {
             expect(error.message).toBeNull();
         }).then(() => {
+            if (server) server.close();
             done();
         });
     });
