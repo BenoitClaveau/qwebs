c4b86430f1fe8dd956deefec7a5cdf00c54a50e4
Benoît Claveau <benoit.claveau@gmail.com>
2016-11-11 17:53:08 +0100

    Test gzip compression

--- tests/services/response.spec.js
+++ tests/services/response.spec.js
@@ -8,9 +8,9 @@
 const Qwebs = require("../../lib/qwebs");
 const http = require("http");
 
-describe("get", () => {
+describe("response", () => {
 
-    it("get", done => {
+    it("redirect", done => {
         let server = null;
         return Promise.resolve().then(() => {
             let $qwebs = new Qwebs({ dirname: __dirname, config: {}});
@@ -43,4 +43,44 @@ describe("get", () => {
             done();
         });
     });
+
+    it("gzip", done => {
+        let server = null;
+        return Promise.resolve().then(() => {
+            let $qwebs = new Qwebs({ dirname: __dirname, config: {}});
+            
+            $qwebs.inject("$info", "./info");
+            $qwebs.get("/get", "$info", "getInfo");
+            
+            return $qwebs.load().then(() => {
+                let promise = new Promise((resolve, reject) => {
+                    server = http.createServer((request, response) => {
+                        return $qwebs.invoke(request, response).then(res => {
+                            resolve();
+                        }).catch(error => {
+                            reject(error);
+                        });
+                    }).listen(1337);
+                });
+                
+                let $client = $qwebs.resolve("$client");
+                let headers = {
+                    'Accept-Encoding': 'gzip'
+                };
+                let request = $client.get({ url: "http://localhost:1337/get", headers: headers }).then(res => {
+                    expect(res.body.whoiam).toBe("I'm Info service.");
+                });
+                return Promise.all([promise, request]);
+            });
+        }).catch(error => {
+            expect(error.stack + JSON.stringify(error.data)).toBeNull();
+        }).then(() => {
+            if (server) server.close();
+            done();
+        });
+    });
 });
+
+var headers = {
+      'Accept-Encoding': 'gzip'
+    };
