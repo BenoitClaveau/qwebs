4580ac615df1de83f0b70c48d2088670cb49abb7
Benoît Claveau <benoit.claveau@gmail.com>
2016-11-11 17:38:51 +0100

    Test redirection

---
+++ tests/services/response.spec.js
@@ -0,0 +1,46 @@
+/*!
+ * qwebs
+ * Copyright(c) 2016 Benoît Claveau
+ * MIT Licensed
+ */
+"use strict";
+
+const Qwebs = require("../../lib/qwebs");
+const http = require("http");
+
+describe("get", () => {
+
+    it("get", done => {
+        let server = null;
+        return Promise.resolve().then(() => {
+            let $qwebs = new Qwebs({ dirname: __dirname, config: {}});
+            
+            $qwebs.inject("$info", "./info");
+            $qwebs.get("/get", "$info", "getInfo");
+            $qwebs.get("/redirect", "$info", "redirect");
+            
+            return $qwebs.load().then(() => {
+                let promise = new Promise((resolve, reject) => {
+                    server = http.createServer((request, response) => {
+                        return $qwebs.invoke(request, response).then(res => {
+                            resolve();
+                        }).catch(error => {
+                            reject(error);
+                        });
+                    }).listen(1337);
+                });
+                
+                let $client = $qwebs.resolve("$client");
+                let request = $client.get({ url: "http://localhost:1337/redirect" }).then(res => {
+                    expect(res.body.whoiam).toBe("I'm Info service.");
+                });
+                return Promise.all([promise, request]);
+            });
+        }).catch(error => {
+            expect(error.stack + JSON.stringify(error.data)).toBeNull();
+        }).then(() => {
+            if (server) server.close();
+            done();
+        });
+    });
+});
