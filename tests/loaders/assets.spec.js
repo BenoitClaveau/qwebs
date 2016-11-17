cb2a18f55784f2b5ce0305d68dee0a4835430435
Benoît Claveau <benoit.claveau@gmail.com>
2016-11-17 10:11:59 +0100

    Test failed to read public folder

--- tests/loaders/assets.spec.js
+++ tests/loaders/assets.spec.js
@@ -10,7 +10,7 @@ const AssetsLoader = require("../../lib/loaders/assets");
 
 describe("assetsLoader", () => {
 
-    it("load", done => {
+    it("Load", done => {
         return Promise.resolve().then(() => {
             let $qwebs = new Qwebs({ dirname: __dirname, config: { folder: "public" }});
             let $config = $qwebs.resolve("$config");
@@ -22,6 +22,22 @@ describe("assetsLoader", () => {
                 expect(assets[1].route).toEqual("/main.html");
             });
             
+        }).catch(error => {
+            expect(error).toBeNull();
+        }).then(() => {
+            done();
+        });
+    });
+    
+    it("Failed to read public folder", done => {
+        return Promise.resolve().then(() => {
+            let $qwebs = new Qwebs({ dirname: __dirname, config: { folder: "public0" }});
+            let $config = $qwebs.resolve("$config");
+            let $router = $qwebs.resolve("$router");
+            
+            return new AssetsLoader($qwebs, $config, $router).load().catch(error => {
+                expect(error.message).toEqual("Failed to read public folder.");            
+            });            
         }).catch(error => {
             expect(error).toBeNull();
         }).then(() => {
