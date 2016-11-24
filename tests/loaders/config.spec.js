f244f2b2db51076ba94843e0d52f2c6b7e1243ef
Benoît Claveau <benoit.claveau@gmail.com>
2016-11-24 21:28:04 +0100

    Update config.spec.js

--- tests/loaders/config.spec.js
+++ tests/loaders/config.spec.js
@@ -16,6 +16,7 @@ describe("configLoader", () => {
             };
             let config = new ConfigLoader($qwebs).create({ folder: "public1" });
             expect(config.folder).toEqual("public1");
+            
         }).catch(error => {
             expect(error.stack).toBeNull();
         }).then(() => {
@@ -30,8 +31,51 @@ describe("configLoader", () => {
             };
             let config = new ConfigLoader($qwebs).create("config.json");
             expect(config.folder).toEqual("public2");
+            let config = new ConfigLoader($qwebs).create("config.json");
         }).catch(error => {
             expect(error.stack).toBeNull();
+        }).then(() => {
+            done();
+        });
+    });
+    
+    it("failed to read file", done => {
+        return Promise.resolve().then(() => {
+             let $qwebs = {
+                root: __dirname
+            };
+            let config = new ConfigLoader($qwebs).create("config.error.json2");
+            fail();
+        }).catch(error => {
+            expect(error.stack).toEqual("Failed to read the configuration file.");
+        }).then(() => {
+            done();
+        });
+    });
+    
+    it("failed to read file", done => {
+        return Promise.resolve().then(() => {
+             let $qwebs = {
+                root: __dirname
+            };
+            let config = new ConfigLoader($qwebs).create("config.error.json");
+            fail();
+        }).catch(error => {
+            expect(error.stack).toEqual("Failed to parse the configuration file.");
+        }).then(() => {
+            done();
+        });
+    });
+    
+    it("error", done => {
+        return Promise.resolve().then(() => {
+            let $qwebs = {
+                root: __dirname
+            };
+            let config = new ConfigLoader($qwebs).create(function() { folder: "public1" });
+            fail();
+        }).catch(error => {
+            expect(error.message).toEqual("Configuration type is not managed.");
         }).then(() => {
             done();
         });
