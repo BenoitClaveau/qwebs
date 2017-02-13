901be73939302c7a4bd0e73b1c70e9649480b808
BenoitClaveau <https://BenoitClaveau@github.com>
2017-02-13 12:04:47 +0100

    Move repository service to utils

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 
 module.exports = require('./lib/qwebs');
 module.exports.DataError = require('./lib/dataerror');
+module.exports.Repository = require('./lib/utils/repository');
 module.exports.MergeStream = require('./lib/utils/stream/mergestream');
 module.exports.ToArray = require('./lib/utils/stream/toarray');
 module.exports.walk = require('./lib/utils/walk');
