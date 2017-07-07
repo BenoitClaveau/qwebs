6b7ed8ff97a2acb9c75325e0127adfc40a63af87
Benoît Claveau <benoit.claveau@izzili.com>
2017-07-07 17:33:22 +0200

    Fix dash to camel case if text contains number

--- tests/utils/string.spec.js
+++ tests/utils/string.spec.js
@@ -18,8 +18,22 @@ describe("StringUtils", () => {
 
     it("camelCaseToDash", done => {
         return Promise.resolve().then(() => {  
+            let res = stringUtils.camelCaseToDash("oauth2Model");
+            expect(res).toEqual("oauth2-model");
+        }).then(done).catch(fail);
+    });
+
+    it("camelCaseToDash", done => {
+        return Promise.resolve().then(() => {  
             let res = stringUtils.dashToCamelCase("test-model");
             expect(res).toEqual("testModel");
+        }).then(done).catch(fail);
+    });
+
+    it("camelCaseToDash", done => {
+        return Promise.resolve().then(() => {  
+            let res = stringUtils.dashToCamelCase("oauth2-model");
+            expect(res).toEqual("oauth2Model");
         }).then(done).catch(fail);
     });
 });
